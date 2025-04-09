a42f0604997188e5e3dae4279b8fb9af7191fac0
Mykha Abarquez <mykha.abarquez@cit.edu>
2025-04-09 21:51:16 +0800

    Connecting Frontend and Backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js', // Ensure PostCSS is configured
+    postcss: './postcss.config.js', 
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
