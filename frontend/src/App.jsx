bcdc7ec57b9571f3a091e1ac809be30c6f6bb0eb
Mykha Abarquez <mykha.abarquez@cit.edu>
2025-04-09 21:39:43 +0800

    Frontend and Backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './assets/Homepage';
+import SignUpType from './assets/SignUpType';
+import RegisterTeacher from './assets/RegisterTeacher';
+import GuessTheWord from './assets/Guesstheword';
+import MemoryGame from './assets/MemoryGame';
+import PaaralanQuest from './assets/PaaralanQuest';
+import ParkeQuest from './assets/ParkeQuest';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpType />} />
+        <Route path="/register-teacher" element={<RegisterTeacher />} />
+
+        {/* Games Section Path */}
+        <Route path="/guesstheword" element={<GuessTheWord />} />
+        <Route path="/memorygame" element={<MemoryGame />} />
+        <Route path="/paaralanquest" element={<PaaralanQuest />} />
+        <Route path="/parkequest" element={<ParkeQuest />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
